675d90a798dcdee3fad7aee8d47f71c3329a092f
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-06 14:39:43 +0300

    Enum constant

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -1,5 +1,11 @@
 package com.cydeo;
 
 public enum Currency {
-    PENNY,NICKLE,DIME,QUARTER
+    PENNY(1),NICKLE(5),DIME(10),QUARTER(25);
+
+    int value;
+
+    Currency(int value) {
+        this.value=value;
+    }
 }
