44b1af13d87e38ab003b9dff496d78d04e467c2c
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-10 18:30:56 +0300

    FunctionalInterfaces added

--- m3-lambda/src/main/java/com/cydeo/FunctionalInterfaces.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterfaces.java
@@ -3,6 +3,8 @@ package com.cydeo;
 import com.cydeo.tasks.farmer.Apple;
 import com.cydeo.tasks.farmer.ApplePredicate;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class FunctionalInterfaces {
@@ -15,12 +17,21 @@ public class FunctionalInterfaces {
             }
         };
 
-        //******************PREDICATE*********************//
+        System.out.println("*****************PREDICATE**************");
 
         Predicate<Integer> lesserThan = i -> i < 18;
 
         System.out.println(lesserThan.test(20));
 
+
+        System.out.println("*****************CONSUMER**************");
+
+        Consumer<Integer> display = i-> System.out.println(i);
+        display.accept(10);
+
+        System.out.println("*****************BI CONSUMER**************");
+        BiConsumer<Integer,Integer>  addTwo = (x,y) ->System.out.println(x+y);
+        addTwo.accept(1,2);
 
     }
 }
