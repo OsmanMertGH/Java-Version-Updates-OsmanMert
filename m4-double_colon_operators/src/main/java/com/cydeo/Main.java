1757774a9dc904970c8e33e8e9dda04c365c6a06
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-11 12:11:11 +0300

    double colon operators

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -23,6 +24,10 @@ public class Main {
         BiFunction<String, Integer, String> func = (str, i) -> str.substring(i);
 
         BiFunction<String, Integer, String> func2 = String::substring;
+
+        Function<Integer, Double> m1 = new MyClass()::method;
+
+        BiFunction<MyClass, Integer, Double> m2 = MyClass::method;
 
     }
 }
