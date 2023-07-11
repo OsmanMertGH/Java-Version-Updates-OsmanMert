7fbab561e99ff49029624abe2587dddddf212a4f
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-11 12:06:39 +0300

    commit

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Main {
@@ -7,5 +8,21 @@ public class Main {
 
 
         Predicate<Integer> pred = (p) -> p % 2 == 0 ? true : false;
+
+        Calculate sum = (x, y) -> System.out.println(x + y);
+
+        Calculate s1 = (x, y) -> Calculator.findSum(x, y);
+
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10, 20);
+
+        Calculator obj = new Calculator();
+        Calculate s3 = obj::findMultiply;
+        s3.calculate(10, 20);
+
+        BiFunction<String, Integer, String> func = (str, i) -> str.substring(i);
+
+        BiFunction<String, Integer, String> func2 = String::substring;
+
     }
 }
