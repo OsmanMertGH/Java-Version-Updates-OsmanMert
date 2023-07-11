bd4f4ebe132ebe7689ab04ff47dcb1cd3ac2d889
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-11 11:42:48 +0300

    calculator

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,8 +1,11 @@
 package com.cydeo;
 
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        Predicate<Integer> pred = (p) -> p % 2 == 0 ? true : false;
     }
 }
