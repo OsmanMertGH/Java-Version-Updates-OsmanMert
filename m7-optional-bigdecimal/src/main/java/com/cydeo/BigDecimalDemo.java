50c3f9a1de3179ce25e5ca9eef324524e8896446
OsmanMertGH <osman_mrt@yahoo.com>
2023-07-24 11:17:39 +0300

    Reducing

---
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/BigDecimalDemo.java
@@ -0,0 +1,23 @@
+package com.cydeo;
+
+import java.math.BigDecimal;
+
+public class BigDecimalDemo {
+    public static void main(String[] args) {
+
+        double amount1 = 374.56;
+        double amount2 = 374.26;
+
+        System.out.println(amount1 - amount2);
+
+        BigDecimal b1 = new BigDecimal("374.56");
+        BigDecimal b2 = new BigDecimal("374.26");
+        BigDecimal b3 = new BigDecimal(374.56);
+        BigDecimal b4 = BigDecimal.TEN;
+        BigDecimal b5 = BigDecimal.ONE;
+
+
+        System.out.println(b1.add(BigDecimal.TEN));
+        System.out.println(b1.subtract(b2));
+    }
+}
--- m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -1,7 +1,32 @@
 package com.cydeo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class OptionalDemo {
     public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2, 4, 2, 10, 23);
+        //empty() - is Present()
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        System.out.println(empty.isPresent());
+
+        System.out.println(Optional.of(number).isPresent());
+
+        //ifPresent
+        Optional<Integer> bigNumber = number.stream().filter(x -> x > 100).findAny();
+        bigNumber.ifPresent(System.out::println);
+
+        //get
+        //System.out.println(bigNumber.get());
+
+        //orElse
+        System.out.println(bigNumber.orElse(5));
+        
 
     }
 }
